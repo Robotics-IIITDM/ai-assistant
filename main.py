46d18434d29d4d2e8366386e09fc3a19d1954d4c
Dhanvanth Saravanan <148760410+Robotics-IIITDM@users.noreply.github.com>
2025-08-04 22:09:46 +0530

    stopnstart

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    return render_template('startrecording.html')
+
+@app.route('/recording', methods=['POST','GET'])
+def recording():
+    return render_template('stoprecording.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
