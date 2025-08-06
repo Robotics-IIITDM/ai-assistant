99f83c7068f9f77e12b5b7574b7d9bdd22161124
Aayushya Paswan <aayushya894@gmail.com>
2025-08-06 13:59:24 +0530

    changes made

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-from flask import *
+from flask import Flask, render_template, request
+from RealtimeSTT import AudioToTextRecorder
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('startrecording.html')
 
-@app.route('/recording', methods=['POST','GET'])
+@app.route('/recording', methods=['GET', 'POST'])
 def recording():
-    return render_template('stoprecording.html')
+    recorder = AudioToTextRecorder(language='en')
+
+    result = {"text": ""}
+
+    def process_text(text):
+        result["text"] = text
+        recorder.stop()  
+    recorder.text(process_text)
+    return render_template('stoprecording.html', text=result["text"])
 
 if __name__ == '__main__':
     app.run(debug=True)
